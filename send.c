b99c907f65eca6ecff86124bbbd7acb1b221fb4c
Ilan Ganor <deadpoolcode@gmail.com>
2017-09-26 01:40:05 +0300

    server_litner: added support for message parameter for send
    Signed-off-by: Ilan Ganor <deadpoolcode@gmail.com>

--- send.c
+++ send.c
@@ -8,7 +8,13 @@ int main(int argc , char *argv[])
     int socket_desc;
     struct sockaddr_in server;
     char *message;
-     
+    //example: ./send write_file:/sys/class/gpio/export=5
+    //example: ./send read_file:/sys/class/gpio/gpio5/value
+    if (argc<2) 
+    {
+	printf("error, usage: ./send [commnad]\n");
+    	return 1;
+    } 
     //Create socket
     socket_desc = socket(AF_INET , SOCK_STREAM , 0);
     if (socket_desc == -1)
@@ -16,7 +22,7 @@ int main(int argc , char *argv[])
         printf("Could not create socket");
     }
          
-    server.sin_addr.s_addr = inet_addr("10.0.0.15");
+    server.sin_addr.s_addr = inet_addr("10.0.0.36");
     server.sin_family = AF_INET;
     server.sin_port = htons( 5797 );
  
@@ -29,14 +35,17 @@ int main(int argc , char *argv[])
      
     puts("Connected now\n");
     //Send some data
-    //message = "write_file:/sys/class/gpio/export=5\n";
-    message = "write_file:test.txt=5\n";
+    message=argv[1];
+    //message = "write_file:/sys/class/gpio/unexport=5\n";
+    //message = "write_file:test.txt=5\n";
     if( send(socket_desc , message , strlen(message) , 0) < 0)
     {
         puts("Send failed");
         return 1;
     }
     puts("Data Send\n");
+
+    
     close(socket_desc);
     return 0;
 }
