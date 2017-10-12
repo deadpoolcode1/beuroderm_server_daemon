555868a7ac9c5439c19a574636b542f64757d738
Ilan Ganor <deadpoolcode@gmail.com>
2017-10-12 15:21:41 +0300

    server_listner: bug send wait forever: added support for timeout
    1. bug description: in case no response from serer socket did not closed
    
    2. bug fix: added definition for closing the port after timeout
    
    3. status: fixed and tested
    
    Signed-off-by: Ilan Ganor <deadpoolcode@gmail.com>

--- send.c
+++ send.c
@@ -7,11 +7,16 @@ int main(int argc , char *argv[])
 {
     int socket_desc;
     struct sockaddr_in server;
+    struct timeval tv;
+	tv.tv_sec = 1;  /* 1 Secs Timeout */
+	tv.tv_usec = 0;  // Not init'ing this can cause strange errors
+	
+
     //example: ./send write_file:/sys/class/gpio/export=5
     //example: ./send read_file:/sys/class/gpio/gpio5/value
-    if (argc<2) 
+    if (argc<3) 
     {
-	printf("error, usage: ./send [commnad]\n");
+	printf("error, usage: ./send [address][commnad]\n");
     	return 1;
     } 
     char *message , server_reply[2000];
@@ -22,10 +27,10 @@ int main(int argc , char *argv[])
         printf("Could not create socket");
     }
          
-    server.sin_addr.s_addr = inet_addr("10.0.0.36");
+    server.sin_addr.s_addr = inet_addr(argv[1]);
     server.sin_family = AF_INET;
     server.sin_port = htons( 5797 );
- 
+ 	setsockopt(socket_desc, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv,sizeof(struct timeval));
     //Connect to remote server
     if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0)
     {
@@ -35,7 +40,7 @@ int main(int argc , char *argv[])
      
     puts("Connected now\n");
     //Send some data
-    message=argv[1];
+    message=argv[2];
     if( send(socket_desc , message , strlen(message) , 0) < 0)
     {
         puts("Send failed");
@@ -44,6 +49,7 @@ int main(int argc , char *argv[])
     puts("Data Send\n");
 
     //Receive a reply from the server
+
     if( recv(socket_desc, server_reply , 2000 , 0) < 0)
     {
         puts("recv failed");
@@ -53,6 +59,8 @@ int main(int argc , char *argv[])
     }
     puts("Reply received:\n");
     puts(server_reply);
+    puts("Data Send1\n");
     close(socket_desc);
+    puts("Data Send2\n");
     return 0;
 }
