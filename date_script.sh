82f258e291940a38751d08bc1c11d0477e05a2fd
ilanmbox@gmail.com <deadpoolcode@gmail.com>
2017-12-22 00:46:42 +0200

    server_listner: added support for rtc setting
    Signed-off-by: ilanmbox@gmail.com <deadpoolcode@gmail.com>

--- date_script.sh
+++ date_script.sh
@@ -2,4 +2,4 @@
 <<description
     this script mediate setting the time from C to the Android system
 description
-eval "date ${1} ; am broadcast -a android.intent.action.TIME_SET;hwclock --hctosys"
+eval "date -u ${1} ; date ${1} ;am broadcast -a android.intent.action.TIME_SET;hwclock --systohc"
